d45e428f173885265582064588e95903dd7a31db
Sandeep Shahu <shahusandeep23@gmail.com>
2026-08-26 01:28:55 +0530

    Add Smallest Missing Multiple of K Solution

--- LeetCode_Daily/SmallestMissingMul.java
+++ LeetCode_Daily/SmallestMissingMul.java
@@ -1,6 +1,17 @@
 package LeetCode_Daily;
 
 import java.util.HashSet;
+/*
+Approach
+    Store all elements of nums in a HashSet for fast lookup.
+    Start checking multiples from k.
+    If the current multiple exists in the set, move to the next multiple by adding k.
+    The first multiple that is not present in the set is the answer.
+
+Complexity
+    Time: O(n + m)
+    Space: O(n)
+*/
 
 public class SmallestMissingMul {
     public static int missingMultiple(int[] nums, int k) {
