9f8fac07fce2f113ab26e2d40ca0ca2a4bd28459
Sandeep Shahu <shahusandeep23@gmail.com>
2026-09-05 00:06:31 +0530

    Add Smallest Stable Index I Solution

--- LeetCode_Daily/SmallestStable.java
+++ LeetCode_Daily/SmallestStable.java
@@ -1,11 +1,20 @@
 package LeetCode_Daily;
 
-/**
- * SmallestStable
+/*
+ Approach
+    Create leftMax[] to store the maximum value from 0 to i.
+    Create rightMin[] to store the minimum value from i to n-1.
+    For each index, calculate:
+    gap = leftMax[i] - rightMin[i]
+    If gap <= k, return the first such index.
+    If no stable index exists, return -1.
+Complexity
+    Time: O(n)
+    Space: O(n)
  */
 public class SmallestStable {
 
-    public int firstStableIndex(int[] nums, int k) {
+    public static  int firstStableIndex(int[] nums, int k) {
         int n=nums.length;
         int[] leftMax=new int[n];
         int[] rightMin=new int[n];
@@ -22,5 +31,10 @@ public class SmallestStable {
             if(gap<=k)return i;
         }
         return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,2,1}; 
+        int k = 1;
+        System.out.println(firstStableIndex(nums,k));
     }
 }
